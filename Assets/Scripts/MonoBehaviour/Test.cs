5f8f3a2e9f9ada81710d22dd27f86f6e58523dce
el-zach <el-zach@web.de>
2019-09-21 09:47:21 +0200

    [ECS] basic test rotating translating cubes

--- Assets/Scripts/MonoBehaviour/Test.cs
+++ Assets/Scripts/MonoBehaviour/Test.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using Unity.Entities;
 using Unity.Transforms;
 using Unity.Rendering;
+using Unity.Mathematics;
+using Unity.Collections;
 
 public class Test : MonoBehaviour
 {
@@ -28,22 +30,59 @@ public class Test : MonoBehaviour
         BasicUnit = entityManager.CreateArchetype(
                 typeof(RenderMesh),
                 typeof(LocalToWorld),
-                typeof(Translation)
+                typeof(Translation),
+                typeof(Rotation),
+                typeof(Scale) //could also be typeof(NonUniformScale) 
             );
     }
 
     [ContextMenu("Execute")]
-    void Execute()
+    void Execute() // exists in Monobehaviour space
     {
-        for (int i = 0; i < count; i++)
+        NativeArray<Entity> entityArray = new NativeArray<Entity>(count,Allocator.Temp); //we dont keep track of the array so having it disposable is good (?)
+
+        entityManager.CreateEntity(BasicUnit,entityArray);
+
+        foreach (Entity entity in entityArray)
         {
-            Entity entity = entityManager.CreateEntity(BasicUnit);
             entityManager.SetSharedComponentData(entity, new RenderMesh
             {
                 mesh = mesh,
                 material = material,
             });
+            entityManager.SetComponentData(entity, new Translation
+            {
+                Value = new float3(0f, UnityEngine.Random.value, 0f)
+            });
+            entityManager.SetComponentData(entity, new Scale
+            {
+                Value = 1f
+            });
         }
     }
 
+}
+
+public class MovementSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach(
+            (ref Translation translation) => //anonymous action
+        {
+            if(translation.Value.y<=10f)
+                translation.Value += new float3(0f, 1f, 0f);
+        });
+    }
+}
+
+public class RotatorSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref Rotation rotation) =>
+        {
+            rotation.Value = quaternion.Euler(0f, 0f, math.PI * Time.realtimeSinceStartup);
+        });
+    }
 }
