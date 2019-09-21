27bc92a6041c111bbd0f11cba61df8d92c2e7c16
el-zach <el-zach@web.de>
2019-09-21 09:14:51 +0200

    [Init] moved folder structure

---
+++ Assets/Scripts/MonoBehaviour/Test.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Transforms;
+using Unity.Rendering;
+
+public class Test : MonoBehaviour
+{
+
+    public int count = 1;
+    public Mesh mesh;
+    public Material material;
+
+    EntityManager entityManager;
+    EntityArchetype BasicUnit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+        Execute();
+    }
+    
+    void Init()
+    {
+        entityManager = World.Active.EntityManager;
+        BasicUnit = entityManager.CreateArchetype(
+                typeof(RenderMesh),
+                typeof(LocalToWorld),
+                typeof(Translation)
+            );
+    }
+
+    [ContextMenu("Execute")]
+    void Execute()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            Entity entity = entityManager.CreateEntity(BasicUnit);
+            entityManager.SetSharedComponentData(entity, new RenderMesh
+            {
+                mesh = mesh,
+                material = material,
+            });
+        }
+    }
+
+}
