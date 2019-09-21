74590372ac95dffe9c24d7d431d93b64a4f53b3c
el-zach <el-zach@web.de>
2019-09-21 12:27:11 +0200

    [Graphics] put in some assets | added some packages

---
+++ Assets/Shader/ShaderRessources/LightingInfo.hlsl
@@ -0,0 +1,17 @@
+void LWRPLightingFunction_float (float3 ObjPos, out float3 Direction, out float3 Color, out float ShadowAttenuation)
+{
+   #ifdef LIGHTWEIGHT_LIGHTING_INCLUDED
+
+      Light light = GetMainLight(GetShadowCoord(GetVertexPositionInputs(ObjPos)));
+      Direction = light.direction;
+      Color = light.color;
+      ShadowAttenuation = light.shadowAttenuation;
+      
+   #else
+
+      Direction = float3(-0.5, 0.5, -0.5);
+      Color = float3(1, 1, 1);
+      ShadowAttenuation = 0.4;
+      
+   #endif
+}
